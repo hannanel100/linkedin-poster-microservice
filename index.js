ebe0c56b3e1858a47a1cb40a8a9a82f1aaddd008
hannanel100 <hannanel100@gmail.com>
2022-11-21 20:59:40 +0200

    working microservice

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // this is a microservice that will read the db, via the api, and then share on linkedin via the linkedin api
 const cron = require("node-cron");
 const axios = require("axios");
+const dayjs = require("dayjs");
+const FormData = require("form-data");
+const API_PORT = "5000";
+console.log(process.env.NODE_ENV);
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? `http://localhost:${API_PORT}`
+    : `https://pigeon-api.onrender.com:${API_PORT}`;
+
 const VISIBILITY = ["PUBLIC", "CONNECTIONS"];
 // schedule cron to run every minute
-cron.schedule("* * * * * ", async () => {
+cron.schedule("* * * * *", async () => {
   console.log("running a task every minute");
-  const usersUrl = "http://localhost:5000/api/users/linkedin/users";
+  const usersUrl = `${baseURL}/api/users/linkedin/users`;
 
   // get the data from the api async/await
   const getData = async (url) => {
@@ -20,72 +29,173 @@ cron.schedule("* * * * * ", async () => {
   };
   let users = await getData(usersUrl);
   //   get all posts for the users
-  const postsUrl = "http://localhost:5000/api/posts";
-  const getPosts = async (url) => {
-    try {
-      const response = await axios.get(url);
-      const { data } = response;
-      //   console.log(data);
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  let { posts } = await getPosts(postsUrl);
-  //   loop through the posts, find the id in the users array, and then post to linkedin
-  posts.forEach(async (post) => {
-    const user = users.find((user) => user.id === post.id);
-    const { accessToken, id } = user;
-    const { content, _id } = post;
-    // TODO: add image later
-
-    const status = await postToLinkedin(content, accessToken, id);
-    console.log(
-      "🚀 ~ file: index.js ~ line 54 ~ posts.forEach ~ status",
-      status
-    );
-    if (status === 200) {
-      // delete the post from the db
-      const deleteUrl = `http://localhost:5000/api/posts/${_id}`;
-      const deletePost = async (url) => {
-        try {
-          const response = await axios.delete(url);
-          const { data } = response;
-          console.log(
-            "🚀 ~ file: index.js ~ line 62 ~ deletePost ~ data",
-            data
-          );
-          return data;
-        } catch (error) {
-          console.log(error);
-        }
+  const usersIds = users.map((user) => user.id);
+  //   loop over the users and get the posts for each user
+  await usersIds.forEach(async (userId, index, array) => {
+    const getPosts = async () => {
+      const options = {
+        method: "GET",
+        url: "http://localhost:5000/api/posts",
+        params: { userId: userId },
       };
-      deletePost(deleteUrl);
-    }
+      try {
+        const res = await axios.request(options);
+        return res.data;
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    let { posts } = await getPosts();
+
+    posts.forEach(async (post) => {
+      const user = users.find((user) => user.id === post.id);
+      const { accessToken, id } = user;
+      const { content, _id, date, isPosted, image } = post;
+      // check if date is past, using dayjs, if yes, post to linkedin, and then delete from db
+      if (!isPosted && dayjs(date).isBefore(dayjs())) {
+        const status = await postToLinkedin(content, accessToken, id, image);
+        console.log(
+          "🚀 ~ file: index.js ~ line 54 ~ posts.forEach ~ status",
+          status
+        );
+        if (status === 201) {
+          // update the post from the db with isPosted: true
+          const updateUrl = `${baseURL}/api/posts/${_id}`;
+          const updatePost = async (url) => {
+            try {
+              const body = {
+                isPosted: true,
+                id: id,
+                content: content,
+                date: date,
+                image: image,
+              };
+              const response = await axios.put(url, body);
+              const { data } = response;
+              console.log(
+                "🚀 ~ file: index.js ~ line 62 ~ deletePost ~ data",
+                data
+              );
+              return data;
+            } catch (error) {
+              console.log(error);
+            }
+          };
+          updatePost(updateUrl);
+        }
+      }
+    });
   });
 });
 
 // function to post to linkedin via the api, this will be called in the cron job, and will post to linkedin, and then delete the post from the db
-const postToLinkedin = async (post, accessToken, id) => {
+const postToLinkedin = async (content, accessToken, id, image = null) => {
   const shareUrl = "https://api.linkedin.com/v2/ugcPosts";
   const headers = {
     "X-Restli-Protocol-Version": "2.0.0",
     Authorization: `Bearer ${accessToken}`,
   };
-  const body = {
-    author: `urn:li:person:8675309${id}`,
-    lifecycleState: "PUBLISHED",
-    specificContent: {
-      shareCommentary: post.content,
-      shareMediaCategory: VISIBILITY[1],
-    },
-    visibility: {
-      "com.linkedin.ugc.MemberNetworkVisibility": "PRIVATE",
-    },
-  };
+  let shareBody = {};
+  if (image) {
+    let uploadUrl = "";
+    let asset = "";
+
+    // Register your image to be uploaded.
+    const registerUploadUrl =
+      "https://api.linkedin.com/v2/assets?action=registerUpload";
+    const body = {
+      registerUploadRequest: {
+        recipes: ["urn:li:digitalmediaRecipe:feedshare-image"],
+        owner: `urn:li:person:${id}`,
+        serviceRelationships: [
+          {
+            relationshipType: "OWNER",
+            identifier: "urn:li:userGeneratedContent",
+          },
+        ],
+      },
+    };
+    try {
+      const registerUploadResponse = await axios.post(registerUploadUrl, body, {
+        headers,
+      });
+      const { data } = registerUploadResponse;
+      uploadUrl =
+        data.value.uploadMechanism[
+          "com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest"
+        ].uploadUrl;
+
+      asset = data.value.asset;
+    } catch (error) {
+      console.error(error);
+    }
+
+    // Upload your image to LinkedIn.
+    try {
+      const imageBuffer = Buffer.from(image, "base64");
+
+      const formData = new FormData();
+      formData.append("fileupload", imageBuffer);
+      const uploadResponse = await axios.put(uploadUrl, formData, {
+        ...headers,
+        ...formData.getHeaders(),
+      });
+      const { data } = uploadResponse;
+      console.log(
+        "🚀 ~ file: index.js ~ line 136 ~ postToLinkedin ~ data",
+        data
+      );
+    } catch (error) {
+      console.error(error);
+    }
+    // Create the image share.
+    shareBody = {
+      author: `urn:li:person:${id}`,
+      lifecycleState: "PUBLISHED",
+      specificContent: {
+        "com.linkedin.ugc.ShareContent": {
+          shareCommentary: {
+            text: content,
+          },
+          shareMediaCategory: "IMAGE",
+          media: [
+            {
+              status: "READY",
+              description: {
+                text: "Center stage!",
+              },
+              media: asset,
+              title: {
+                text: "LinkedIn Talent Connect 2021",
+              },
+            },
+          ],
+        },
+      },
+      visibility: {
+        "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC",
+      },
+    };
+  } else {
+    shareBody = {
+      author: `urn:li:person:${id}`,
+      lifecycleState: "PUBLISHED",
+      specificContent: {
+        "com.linkedin.ugc.ShareContent": {
+          shareCommentary: {
+            text: content,
+          },
+          shareMediaCategory: "NONE",
+        },
+      },
+      visibility: {
+        "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC",
+      },
+    };
+  }
 
   try {
-    const response = await axios.post(shareUrl, body, {
+    const response = await axios.post(shareUrl, shareBody, {
       headers: headers,
     });
     console.log(response);
